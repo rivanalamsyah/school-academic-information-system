672f326cbc5bf7abd56245fa476191ca858232f7
Rivan Alamsyah <95959517+rivanalamsyah@users.noreply.github.com>
2026-07-30 09:16:30 +0700

    fix: convert netlify functions utils/db to ESM

--- netlify/functions/utils/db.js
+++ netlify/functions/utils/db.js
@@ -1,5 +1,9 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const DB_PATH = path.join(__dirname, '..', '..', 'src', 'db', 'data.json');
 
@@ -26,6 +30,6 @@ function loadDB() {
   return cached;
 }
 
-module.exports = {
-  getDB: loadDB,
-};
+export function getDB() {
+  return loadDB();
+}
